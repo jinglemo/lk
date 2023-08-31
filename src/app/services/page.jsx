b9e1404b63a0ee2fffd0058b7cb652cde110346c
Jing Mo <Jingmo39@gmail.com>
2023-08-31 17:08:07 -0400

    image fix

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+
 import React from "react";
 import Prints from "@components//Prints";
 import Desi from "@components//Desi";
--- src/components/Desi.jsx
+++ src/components/Desi.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { Card } from "flowbite-react";
-import Image from "next/link";
+import Image from "next/image";
 
 const Desi = () => {
   return (
