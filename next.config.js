f4c3c02217238a6d412f445b2f9dc808aa41bf25
Jing Mo <Jingmo39@gmail.com>
2023-08-31 16:58:44 -0400

    webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    future: { webpack5: true }
      // Set to true or false based on your preference
     // Other configuration options
   };
