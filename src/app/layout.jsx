5b804a33ede4ecb5872bdf07156f78392fda7f00
Jing Mo <Jingmo39@gmail.com>
2023-09-06 15:15:51 -0400

    icon added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,8 @@
-import Head from 'next/head'
 import './globals.css'
 import { Lora } from 'next/font/google'
 import Navbar from '@components//Navbar'
 import Foot from '@components//Footer'
-
+import Head from 'next/head'
 const lora = Lora({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,15 +14,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" /> {/* Update the href attribute accordingly */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="icon.ico" sizes="<generated>" /> {/* Use an absolute path to the favicon */}
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
       <body className={lora.className}>
+        
         <Navbar />
         {children}
         <Foot />
-        </body>
+      </body>
     </html>
   )
 }
+
